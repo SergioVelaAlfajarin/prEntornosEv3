6f5fd7f0433081f300ef374cfbc2087dc83720b3
Sergio Vela <102852790+SergioVelaAlfajarin@users.noreply.github.com>
2022-05-03 20:22:42 +0200

    Update TrabajoTest.java

--- test/TestModelo/TrabajoTest.java
+++ test/TestModelo/TrabajoTest.java
@@ -17,5 +17,70 @@ import static org.junit.Assert.*;
  * @author alumno
  */
 public class TrabajoTest {
-
+Trabajo trabajo1;
+    CuentaBanco cuenta;
+    Usuario usuario1;
+    
+    
+    public TestTrabajo() {
+        
+    }
+    
+    @Before
+    public void setUp() {
+        usuario1 = new Usuario("Paco","Ramirez");
+        cuenta = new CuentaBanco(usuario1);
+        trabajo1 = new Trabajo("Maderero",1230);
+    }
+    
+    @Test
+    public void TestCreacion(){
+        //Estos tests comprueban que el constructor lanze una excepcion en las condiciones adecuadas.
+        try{
+            trabajo1 = new Trabajo(new String(),900);
+            fail();
+        }catch(IllegalArgumentException e){}
+        
+        try{
+            trabajo1 = new Trabajo("ej",900);
+            fail();
+        }catch(IllegalArgumentException e){}
+        try{
+            trabajo1 = new Trabajo("Maderero",120);
+            fail();
+        }catch(IllegalArgumentException e){}
+        try{
+            trabajo1 = new Trabajo(new String(),Double.MAX_VALUE+1);
+            fail();
+        }catch(IllegalArgumentException e){}
+        //Por ultimo lo incializamos bien
+        trabajo1 = new Trabajo("Maderero",1230);
+    }
+    
+    @Test
+    public void TestTrabajar(){
+        
+        //Comprobamos que lanze una excepcion si le pasamos un objeto nulo
+        try{
+            CuentaBanco cuentaMala = null;
+            trabajo1.trabajar(cuentaMala);
+            fail();
+        }catch(IllegalArgumentException |NullPointerException e){}
+        
+        //Comprobamos el comportamiento del metodo
+        
+        double saldoBefore = cuenta.getSaldo();
+        trabajo1.trabajar(cuenta);
+        double saldoExpected = saldoBefore+trabajo1.getBonusAntiguedad()+trabajo1.getSueldo();
+        assertEquals(saldoExpected,cuenta.getSaldo(),1);
+    }
+    
+    @Test
+    public void TestAddAntiguedad(){
+        usuario1 = new Usuario("Paco","Ramirez");
+        cuenta = new CuentaBanco(usuario1);
+        double salariopre = trabajo1.getBonusAntiguedad();
+        trabajo1.addMesAntiguedad();
+        assertEquals(salariopre+100, trabajo1.getBonusAntiguedad(),1);
+    }
 }
